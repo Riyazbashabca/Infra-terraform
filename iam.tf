55823da0a53d19c3bb1ca44e9c6165854e10660a
Riyaz Basha <riyazbasha.bca@gmail.com>
2026-05-19 06:48:42 +0530

    backend configured

--- iam.tf
+++ iam.tf
@@ -1,4 +1,9 @@
-provider "azuread" {}
+provider "azuread" {
+  
+  client_id        = "03c431f4-72e6-489c-8855-ea12afa4ead5"
+  client_secret    = "6iF8Q~yDgX7ahRn8VFp6enBe.4sUJgQ4C34GtcYw"
+  tenant_id        = "20ade2b2-2ce5-4299-8e61-44025f44b78b"
+}
 
 resource "azuread_group" "devops_team" {
   display_name     = "DevOps-Team"
